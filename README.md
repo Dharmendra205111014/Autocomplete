e450b2a985ab769328f5fb4aa151b1dd7fb9feaf
Dharmendra <dharmendra.patel26@gmail.com>
2017-03-26 23:34:46 +0530

    First commit with basic working feature

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Autocomplete
+# Development is in progress !!!
+
+This is a small javascript lib to create simple autocomplete based on list provided already
---
+++ autocomplete.js
@@ -0,0 +1,65 @@
+var autocomplete = function (obj) {
+  this.dataStore = obj.dataArray || [];
+  this.inputBox = document.getElementById(obj.inputBox);
+  this.async = obj.Async || false;
+  this.dataInputBox = obj.doc || {};
+  this.suggestionBox = undefined;
+  this.minSuggestionChar = obj.minSuggestionChar || 3;
+  this.metaData = {};
+}
+
+autocomplete.prototype.createTemplate = function() {
+  var i = document.createElement('input');
+  i.setAttribute('class', 'autocompleteInput');
+  i.id = this.inputBox.id + 'InputBox';
+  var d = document.createElement('div');
+  d.setAttribute('class', 'autocompleteSelectBox');
+  d.id = this.inputBox.id + 'SuggestionBox';
+
+  this.inputBox.appendChild(i);
+  this.inputBox.appendChild(d);
+  this.dataInputBox = i;
+  this.suggestionBox = d;
+
+};
+
+autocomplete.prototype.display = function() {
+  this.createTemplate();
+  var self = this;
+  document.getElementById(this.inputBox.id+'InputBox').addEventListener('keyup', function() {
+    var val = this.value;
+    self.getInputText.call(self, val);
+  });
+};
+
+autocomplete.prototype.getInputText = function(val) {
+  this.suggestionBox.innerHTML = ""
+  if(!this.metaData[val] && this.minSuggestionChar <= val.length) {
+    this.createMetaData(val);
+  }
+  this.buildSuggestionBox(val);
+};
+
+autocomplete.prototype.buildSuggestionBox = function(val) {
+  var suggestions = this.metaData[val];
+  var result;
+  if(suggestions && suggestions.length > 0) {
+    result = "<ul>";
+    for (var i=0; i<suggestions.length; i++) {
+      result += '<li>'+ suggestions[i] +'</li>'
+    }
+    result+='</ul>';
+    this.suggestionBox.innerHTML = result;
+  }
+}
+
+autocomplete.prototype.createMetaData = function(val) {
+  var resultArr = [];
+  for (var i=0; i<this.dataStore.length; i++) {
+    var str = this.dataStore[i];
+    if (str.indexOf(val) !== -1) {
+      resultArr.push(this.dataStore[i]);
+    }
+  }
+  this.metaData[val] = resultArr;
+};
